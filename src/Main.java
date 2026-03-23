d7d2f7a290ab8abbd394423749994d91b8561f27
路镒萌 <2025317220424@webmail.hzau.edu.cn>
2026-03-23 21:10:42 +0800

    接口设计

--- src/Main.java
+++ src/Main.java
@@ -51,8 +51,6 @@ public class Main {
                     }
                 });
         System.out.println(fib2(64));
-               //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
-        // 查看 IntelliJ IDEA 建议如何修正。
 
     }
 }
